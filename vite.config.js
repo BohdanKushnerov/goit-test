cf55daf45918eca93c974c356c28ca397cfd35fb
Bohdan Kushnerov <bohdan.kushnerov@gmail.com>
2023-05-19 21:11:50 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/goit-test",
+  route: "/goit-test/tweets",
   plugins: [react()],
 });
